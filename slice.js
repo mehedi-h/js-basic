6968ada3c94594e98b907e71b7a2d1748e4c8023
mehedi-h <mehedihs2015@gmail.com>
2023-08-05 21:48:42 +0600

    slice example

---
+++ slice.js
@@ -0,0 +1,12 @@
+
+const number = [10,20,30,40,50,60,70,80,90,100];
+const myName = ['Amar', 'Sonar', 'Bangla', 'Ami', 'Tomay', 'Valobashi'];
+
+console.log(number.includes(50));
+console.log(number.includes(55));
+
+const sliceNumber = number.slice(4,8);
+const sliceName = myName.slice(3, 6);
+
+console.log(sliceNumber);
+console.log(sliceName);
