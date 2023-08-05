7268bab2e9a2bb1bd5e956da914c6c613faf4473
mehedi-h <mehedihs2015@gmail.com>
2023-08-05 17:45:42 +0600

    modified alternative

--- cake.js
+++ cake.js
@@ -1,3 +1,4 @@
+/*
 const batashi = 84;
 const glumulu = 97;
 const chinku = 99;
@@ -13,3 +14,36 @@ else if (glumulu > batashi && glumulu > chinku) {
 else {
     console.log('Chinku Will get the tasty cake')
 }
+
+//task 1-----
+function getCake(cake) {
+    const batashi = 84;
+    const glumulu = 97;
+    const chinku = 99;
+
+    //console.log(batashi, glumulu, chinku)
+
+    if (batashi > glumulu && batashi > chinku) {
+        console.log('Batashi will get the tasty cake')
+    } 
+    else if (glumulu > batashi && glumulu > chinku) { 
+        console.log('Glumulu will get the tasty cake')
+    }
+    else {
+        console.log('Chinku Will get the tasty cake')
+    }
+}
+getCake();
+*/
+function cakeWinner (cake) {
+    let batashi = 84;
+    let glumulu = 97;
+    let chinku = 99;
+
+    const winner = Math.max(batashi, glumulu, chinku);
+    //console.log(batashi, glumulu, chinku)
+    //console.log(winner);
+    return winner;
+}
+const cake = cakeWinner();
+console.log(cake);
