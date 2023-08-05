a53eb3b56e74bf81d8a5b0ad51b90e1693749724
mehedi-h <mehedihs2015@gmail.com>
2023-08-05 17:13:19 +0600

    modified

---
+++ cake.js
@@ -0,0 +1,15 @@
+const batashi = 84;
+const glumulu = 97;
+const chinku = 99;
+
+//console.log(batashi, glumulu, chinku)
+
+if (batashi > glumulu && batashi > chinku) {
+    console.log('Batashi will get the tasty cake')
+} 
+else if (glumulu > batashi && glumulu > chinku) { 
+    console.log('Glumulu will get the tasty cake')
+}
+else {
+    console.log('Chinku Will get the tasty cake')
+}
